7f1558521b9b84a47342336741d1bf8e86ab58fa
brillientgirl170 <brillientgirl170@gmail.com>
2021-05-26 22:32:14 +0600

    code rearrange blogController

--- app/Http/Controllers/Blog/BlogController.php
+++ app/Http/Controllers/Blog/BlogController.php
@@ -12,106 +12,125 @@ use Session;
 
 class BlogController extends Controller
 {
-
     public function index()
     {
-      $blogs = Blog::where('author_id', Auth::user()->id)->where('status', 1)->get();
-      return view('blog.index', ['blogs' => $blogs]);
+        $data['blogs'] = Blog::where(['author_id' => Auth::user()->id, 'status' => 1])->get();
+        return view('blog.index', $data);
     }
 
     public function create(){
-      $bcategories = BlogCategory::all();
-      return view('blog.create-blog', ['bcategories' => $bcategories]);
+        $data['bcategories'] = BlogCategory::all();
+
+        return view('blog.create-blog', $data);
     }
 
     public function blog(){
-      $blogs = Blog::all();
-      return view('blog.blogs', ['blogs' => $blogs]);
+        $data['blogs'] = Blog::all();
+        return view('blog.blogs', $data);
     }
 
     public function store(Request $request)
     {
       
-      $blogSlug = $request->title.rand();
-      $slug = Str::slug($blogSlug, '-');
+        $blogSlug = $request->title.rand();
+        $slug = Str::slug($blogSlug, '-');
 
-      $blog = new Blog();
-      $blog->author_id = Auth::user()->id; 
-      $blog->title = $request->title;
-      $blog->category_id = $request->category_id;
-      $blog->description = $request->description;
-      $blog->content = $request->content;
-      $blog->blog_slug = $slug;
+        $blog = new Blog();
+        $blog->author_id = Auth::user()->id; 
+        $blog->title = $request->title;
+        $blog->category_id = $request->category_id;
+        $blog->description = $request->description;
+        $blog->content = $request->content;
+        $blog->blog_slug = $slug;
 
-      if($request->hasFile('image')){
-          $extension = $request->file('image')->getClientOriginalExtension();
-          $fileNameToStore =time().'.'.$extension;
-          $path = $request->file('image')->storeAs('public/uploads/', $fileNameToStore);
-          $blog->image = $fileNameToStore;
-          $blog->save();
-        return Redirect('/my/article');
-      }else{
-        return Redirect()->back();
-      }
+        if ($request->hasFile('image')) {
+            $extension = $request->file('image')->getClientOriginalExtension();
+            $fileNameToStore =time().'.'.$extension;
+            $path = $request->file('image')->storeAs('public/uploads/', $fileNameToStore);
+            $blog->image = $fileNameToStore;
+            $blog->save();
 
-      
+            return Redirect('/my/article');
+        } else {
+            return Redirect()->back();
+        }
 
-      return back();
+        return back();
 
     }
 
     public function details($slug)
     {
-      $moreBlogs = Blog::where('status', 1)->latest()->limit(4)->get();
-      $singleBlog = Blog::where('blog_slug', $slug)->where('status', 1)->first();
-      $blogKey = 'blog_'. $singleBlog->id;
-      if(!Session::has($blogKey)){
-        $singleBlog->increment('views');
-        Session::put($blogKey, 1);
-      }
-      $recentBlog = Blog::where('status', 1)->orderBy('id', 'asc')->limit(3)->get();
-      $popularBlog = Blog::where('status', 1)->orderBy('views', 'desc')->limit(3)->get();
+        $data['moreBlogs'] = Blog::where('status', 1)->latest()->limit(4)->get();
+        $data['singleBlog'] = $singleBlog = Blog::where(['blog_slug' => $slug, 'status' => 1])->first();
 
-      return view('blog.blog-deatils', ['singleBlog' => $singleBlog, 'moreBlogs' => $moreBlogs, 'recentBlog' => $recentBlog, 'popularBlog' => $popularBlog]);
+        if (empty($singleBlog)) {
+            return back(); 
+        } else {
+            $data['blogKey'] = $blogKey = 'blog_'. $singleBlog->id;
+            if (!Session::has($blogKey)) {
+              $singleBlog->increment('views');
+              Session::put($blogKey, 1);
+            }
+            $data['recentBlog'] = Blog::where('status', 1)->orderBy('id', 'asc')->limit(3)->get();
+            $data['popularBlog'] = Blog::where('status', 1)->orderBy('views', 'desc')->limit(3)->get();
+
+            return view('blog.blog-deatils', $data);
+        }
     }
 
    public function deleteblog($id)
    {
-      Blog::find($id)->delete();
-      return redirect()->back();
+        $blog = Blog::find($id);
+        if (empty($blog)) {
+            return  redirect('/my/article');
+        } else {
+            $blog->delete();
+        }
+
+        return redirect()->back();
    }
 
    public function editblog($id)
    {
-     $bcategories = BlogCategory::all();
-     $blogInfo = Blog::where('id', $id)->first();
-     return view('blog.edit-blog', ['bcategories' => $bcategories, 'blogInfo' => $blogInfo]);
+        $data['bcategories'] = BlogCategory::all();
+        $data['blogInfo'] = Blog::where('id', $id)->first();
+
+        if (empty($data['blogInfo'])) {
+            return redirect('/my/article');
+        }
+
+        return view('blog.edit-blog', $data);
    }
 
    public function updateblog(Request $request, $id)
    {
-      $blogSlug = $request->title.rand();
-      $slug = Str::slug($blogSlug, '-');
+        $blogSlug = $request->title.rand();
+        $slug = Str::slug($blogSlug, '-');
 
-      $blog = Blog::find($id);
-      $blog->author_id = Auth::user()->id; 
-      $blog->title = $request->title;
-      $blog->category_id = $request->category_id;
-      $blog->description = $request->description;
-      $blog->content = $request->content;
-      $blog->blog_slug = $slug;
-      $image = $request->image;
+        $blog = Blog::find($id);
+        if (!empty($blog)) {
+            $blog->author_id = Auth::user()->id; 
+            $blog->title = $request->title;
+            $blog->category_id = $request->category_id;
+            $blog->description = $request->description;
+            $blog->content = $request->content;
+            $blog->blog_slug = $slug;
+            $image = $request->image;
 
-      if($request->hasFile('image')){
-        $extension = $request->file('image')->getClientOriginalExtension();
-          $fileNameToStore =time().'.'.$extension;
-          $path = $request->file('image')->storeAs('public/uploads/', $fileNameToStore);
-          $blog->image = $fileNameToStore;
-          $blog->save();
-      }else{
-          $blog->save();
-      }
+            if ($request->hasFile('image')) {
+                $extension = $request->file('image')->getClientOriginalExtension();
+                $fileNameToStore =time().'.'.$extension;
+                $path = $request->file('image')->storeAs('public/uploads/', $fileNameToStore);
+                $blog->image = $fileNameToStore;
+                $blog->save();
+            } else {
+                $blog->save();
+            }
 
-      return redirect()->back();
+            return redirect()->back();
+        } else {
+            return back();
+        }
    }
 }
